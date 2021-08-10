d09d5bbc6543f2aac08a3070b525a6ab5b8bfca1
Grosheny Michael <grosheny.michael@gmail.com>
2021-08-10 15:49:23 +0200

    ex2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.destroy_all
+Task.create!(title: 'test', details: 'for smthg')
+Task.create!(title: 'test2', details: 'for smthg again')
